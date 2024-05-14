b2553e663197d80afb23f9d239781f8e1b0dcd6f
Anutej Kardele <anutej.kardele@gmail.com>
2024-05-14 18:23:41 +0530

    working on screw driver

--- Assets/Project/Scripts/Screw/Screw.cs
+++ Assets/Project/Scripts/Screw/Screw.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Screw : MonoBehaviour
 {
     private Rigidbody rb;
     private XRGrabInteractable xRGrabInteractable;
+
+    public float screwInsert = -0.02414f;
 
     private void Start(){
         rb = GetComponent<Rigidbody>();
@@ -19,6 +22,12 @@ public class Screw : MonoBehaviour
             transform.eulerAngles = other.transform.eulerAngles;
             transform.GetChild(0).GetComponent<Collider>().isTrigger = true;
             Destroy(other.gameObject);
+            
+            screwSnapPosition = transform.position;
+            Vector3 screwPos = screwSnapPosition;
+            screwPos.z += screwInsert;
         }
     }
+
+    private Vector3 screwSnapPosition, screwTightPosition;
 }
--- Assets/Project/Scripts/Screw/ScrewDriver.cs
+++ Assets/Project/Scripts/Screw/ScrewDriver.cs
@@ -1,19 +1,40 @@
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Device;
 
 public class ScrewDriver : MonoBehaviour
 {
+    [SerializeField] private InputActionProperty triggerLeft, triggerRight;
+    [SerializeField] private float triggerValueL, triggerValueR;
     private Rigidbody rb;
     private XRGrabInteractable xRGrabInteractable;
+
+    public Hand handGrabbed;
 
     private void Start(){
         rb = GetComponent<Rigidbody>();
         xRGrabInteractable = GetComponent<XRGrabInteractable>();
+        xRGrabInteractable.selectExited.AddListener(OnRealease);
+        xRGrabInteractable.selectEntered.AddListener(OnSelectEntering);
+    }
+
+    private Screw screw;
+
+    private void Update(){
+        triggerValueL = triggerLeft.action.ReadValue<float>();
+        triggerValueR = triggerRight.action.ReadValue<float>();
+
+        if((triggerValueL > 0)&& screw != null){
+            // move screw behind till it crosses the max limit and end later 
+        }
     }
 
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.tag == "Screw"){
-            Debug.Log("Screw --------------------------------");
+
+            xRGrabInteractable.trackPosition = false;
+            xRGrabInteractable.trackRotation = false;
 
             transform.position = other.transform.position;
             Vector3 rot = transform.eulerAngles;
@@ -21,8 +42,24 @@ public class ScrewDriver : MonoBehaviour
             rot.y = 0;
             transform.eulerAngles = rot;
 
-            rb.constraints |= RigidbodyConstraints.FreezePosition;
-            rb.constraints |= RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+            screw = other.transform.parent.GetComponent<Screw>();
         }
     }
+
+    private void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.name == "LeftHand") handGrabbed = Hand.LeftHand;
+        else if (args.interactorObject.transform.name == "RightHand") handGrabbed = Hand.RightHand;
+
+    }
+
+    private void OnRealease(SelectExitEventArgs arg0){
+        xRGrabInteractable.trackPosition = true;
+        xRGrabInteractable.trackRotation = true;
+        handGrabbed = Hand.None;
+    }
+}
+
+public enum Hand{
+    None, LeftHand, RightHand
 }
